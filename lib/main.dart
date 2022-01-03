edeea2dac0ee8db6b247bdd3413bb2c332b9157f
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-01-03 16:42:13 +0100

    start screen done

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonContainer(const Color(0xffFFA450), 'Register'),
+        _buildButtonContainer(const Color(0xffED6436), 'Log in')
+      ],
+    );
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
+      title: 'StartScreen',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                'assets/images/logo.png',
+                semanticLabel: 'logo',
+                width: 400,
+                height: 280,
+              ),
+              height: 700,
+              alignment: Alignment.center,
+            ),
+            buttonSection,
+          ],
         ),
-        body: const Center(
-          child: Text('Hello World'),
+      ),
+    );
+  }
+
+  Container _buildButtonContainer(Color color, String label) {
+    return Container(
+      width: 172,
+      height: 54,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28.0),
+        color: color,
+      ),
+      child: Center(
+        child: Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
         ),
       ),
     );
