a720677972568a31f6f676814b4fc9391d5ef3a9
Vertix1o <piotrek.vladimir@gmail.com>
2022-01-05 00:41:56 +0100

    Added search list style

--- lib/Screens/Search/components/body.dart
+++ lib/Screens/Search/components/body.dart
@@ -1,10 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mobilki/components/button.dart';
 import 'package:mobilki/components/input_field.dart';
 import 'package:mobilki/constants.dart';
-import 'dart:developer';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
@@ -24,11 +22,10 @@ class _BodyState extends State<Body> {
 
   String _search_value = "";
 
-
   @override
   Widget build(BuildContext context) {
     final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('users').snapshots();
+        FirebaseFirestore.instance.collection('users').snapshots();
     Size size = MediaQuery.of(context).size;
     return Center(
         child: Column(
@@ -48,7 +45,7 @@ class _BodyState extends State<Body> {
             hintText: "Name",
             onChanged: (value) {
               setState(() {
-                  _search_value = value.toLowerCase();
+                _search_value = value.toLowerCase();
               });
             },
           ),
@@ -56,27 +53,63 @@ class _BodyState extends State<Body> {
               stream: _usersStream,
               builder: (BuildContext context,
                   AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (_search_value.length<3) {
+                if (_search_value.length < 3) {
                   return const SizedBox.shrink();
                 }
                 if (snapshot.hasError) {
-                  return Text('Something went wrong');
+                  return const Text('Something went wrong');
                 }
 
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const CircularProgressIndicator();
                 }
-                if (!snapshot.hasData) return Text("No results found");
+                if (!snapshot.hasData) return const Text("No results found");
                 var data = snapshot.data!.docs;
-                data = data.where((x)=>(x['name'] as String).contains(_search_value)).toList();
+                data = data
+                    .where((x) => (x['name'] as String).contains(_search_value))
+                    .toList();
                 var count = data.length;
                 return Expanded(
                     child: ListView.separated(
+                  padding: const EdgeInsets.all(8),
                   itemBuilder: (BuildContext context, int index) {
-                    return Container(
+                    return SizedBox(
                         height: 50,
-                        color: orange,
-                        child: Text(((data[index]['name']) as String).toTitleCase()));
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                                flex: 4,
+                                child: Container(
+                                    width: 40,
+                                    height: 40,
+                                    decoration: const BoxDecoration(
+                                        shape: BoxShape.circle, color: orange),
+                                    child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text((data[index]['name'][0]+data[index]['name'][(data[index]['name'] as String).indexOf(' ')+1] as String).toUpperCase(),
+                                              style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.w300,
+                                                  fontSize: 18))
+                                        ]))),
+                            Expanded(
+                                flex: 13,
+                                child: Text(
+                                    ((data[index]['name']) as String)
+                                        .toTitleCase(),
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 18))),
+                            Expanded(
+                                flex: 3,
+                                child: Container(child: const Icon(Icons.add)))
+                          ],
+                        ));
                   },
                   itemCount: count,
                   separatorBuilder: (BuildContext context, int index) =>
