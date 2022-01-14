54c30a83c2a93c60928b64940f7ebfe2edbdb89f
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-01-14 14:24:29 +0100

    logout placeholder implemented

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mobilki/Screens/Login/login_screen.dart';
 import 'package:mobilki/screens/Home/components/body.dart';
 import 'package:mobilki/components/navbar.dart';
+import 'package:mobilki/resources/auth_methods.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,11 +10,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Games'),
-      ),
-      body: Body(), 
-      bottomNavigationBar: Navbar(index: 0)
-    );
+        appBar: AppBar(
+          title: Text('Games'),
+          leading: IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => {
+              AuthMethods().logout(),
+              Navigator.of(context).pushAndRemoveUntil(
+                  new MaterialPageRoute(builder: (context) => LoginScreen()),
+                  (route) => false)
+            },
+          ),
+        ),
+        body: Body(),
+        bottomNavigationBar: Navbar(index: 0));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:mobilki/screens/Home/home_screen.dart';
 import 'package:mobilki/screens/Start/start_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'screens/Home/home_screen.dart';
 import 'screens/Login/login_screen.dart';
@@ -20,14 +21,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    User? _user = FirebaseAuth.instance.currentUser;
+    Widget firstScreen;
+    if (_user != null) {
+      firstScreen = const HomeScreen();
+    } else {
+      firstScreen = const StartScreen();
+    }
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'StartScreen',
+        title: 'BasketballAppka',
         theme: ThemeData(
             textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme)),
         initialRoute: '/',
         routes: {
-          '/': (context) => const StartScreen(),
+          '/': (context) => firstScreen,
           '/register': (context) => const RegisterScreen(),
           '/login': (context) => const LoginScreen(),
           '/search': (context) => const SearchScreen(),
